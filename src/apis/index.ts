a546670e287dbe801cab57d0766526e091e91f51
VersaXu <62047735+VersaXu@users.noreply.github.com>
2022-09-20 21:13:33 +0800

    Update index.ts

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,17 +3,5 @@ import { request } from '@/utils/request'
 export function apiTest() {
   return request({
     url: '/test'
-    sadsajkhgjshghgsafdhga
-    fdgfdgdf
-      
-    drgfd
-    g 
-    d
-    d
-
-    df
-     
-    fdg
-    fd
   })
 }
