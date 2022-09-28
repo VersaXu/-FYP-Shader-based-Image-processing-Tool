1b5f2023eff6d1d997e1bc411fd94e6d0e15a5c7
VersaXu <62047735+VersaXu@users.noreply.github.com>
2022-09-28 16:58:08 +0800

    图像编辑页面三种上传方式

--- src/pages/ImageProcessing/TotalPackage/index.tsx
+++ src/pages/ImageProcessing/TotalPackage/index.tsx
@@ -1,10 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageContainer from '@/components/PageContainer'
-import { Card } from 'antd'
+import { InboxOutlined, UploadOutlined } from '@ant-design/icons'
+import { Button, Card, message, Upload } from 'antd'
+
+import ImgCrop from 'antd-img-crop'
+import type { RcFile, UploadFile, UploadProps } from 'antd/es/upload/interface'
+
+const { Dragger } = Upload
+
+const props = {
+  name: 'file',
+  multiple: true,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+
+  onChange(info: any) {
+    const { status } = info.file
+
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList)
+    }
+
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`)
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`)
+    }
+  },
+
+  onDrop(e: any) {
+    console.log('Dropped files', e.dataTransfer.files)
+  }
+}
+
 const TotalPackage: React.FC = () => {
+  const [fileList, setFileList] = useState<UploadFile[]>([
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+    }
+  ])
+
+  const onChange: UploadProps['onChange'] = ({ fileList: newFileList }) => {
+    setFileList(newFileList)
+  }
+
+  const onPreview = async (file: UploadFile) => {
+    let src = file.url as string
+    if (!src) {
+      src = await new Promise(resolve => {
+        const reader = new FileReader()
+        reader.readAsDataURL(file.originFileObj as RcFile)
+        reader.onload = () => resolve(reader.result as string)
+      })
+    }
+    const image = new Image()
+    image.src = src
+    const imgWindow = window.open(src)
+    imgWindow?.document.write(image.outerHTML)
+  }
   return (
     <PageContainer>
-      <Card>这里应该生成一个canvas，然后周围有可以对此进行操作的按钮函数</Card>
+      <Card>
+        <Card>
+          {' '}
+          <Dragger {...props}>
+            <p className='ant-upload-drag-icon'>
+              <InboxOutlined />
+            </p>
+            <p className='ant-upload-text'>Click or drag file to this area to upload</p>
+            <p className='ant-upload-hint'>
+              Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files
+            </p>
+          </Dragger>
+          <br />
+          <br />
+          <Upload action='https://www.mocky.io/v2/5cc8019d300000980a055e76' listType='picture' maxCount={1}>
+            <Button icon={<UploadOutlined />}>Upload (Max: 1)</Button>
+          </Upload>
+          <br />
+          <br />
+          <ImgCrop rotate>
+            <Upload
+              action='https://www.mocky.io/v2/5cc8019d300000980a055e76'
+              listType='picture-card'
+              fileList={fileList}
+              onChange={onChange}
+              onPreview={onPreview}
+            >
+              {fileList.length < 5 && '+ Upload'}
+            </Upload>
+          </ImgCrop>
+        </Card>
+      </Card>
     </PageContainer>
   )
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,34 @@
 import React from 'react'
-import { Button, Card } from 'antd'
+import { Button, Card, Carousel } from 'antd'
 import styles from './index.module.less'
 import PageContainer from '@/components/PageContainer'
+
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79'
+}
+
 const HomePage: React.FC = () => {
   return (
     <PageContainer showBreadcrumb={false}>
       <Card>
-        首页
-        <h1>这里要写一个功能展示界面</h1>
+        <Carousel effect='fade' autoplay>
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
       </Card>
     </PageContainer>
   )
